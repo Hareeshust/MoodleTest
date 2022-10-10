a142826b8f9f55c1dd40418fb33fbfb25250f2b2
HareeshUST <hareesh.gopi@ust.com>
2022-10-10 22:10:01 +0530

    Update Login.tsx

--- src/modules/Auth/pages/Login.tsx
+++ src/modules/Auth/pages/Login.tsx
@@ -83,124 +83,124 @@ function Login(props: any) {
         });
   }
   return (
-    // <div className="auth-inner">
-    //     <button onClick={signInWithGoogle} type="button" className="login-with-google-btn" >
-    //       Sign in with Google
-    //     </button>
+    <div className="auth-inner">
+        <button onClick={signInWithGoogle} type="button" className="login-with-google-btn" >
+          Sign in with Google
+        </button>
 
-    //     {/* <button type="button" className="login-with-google-btn" disabled>
-    //       Sign in with Google
-    //     </button> */}
-    //     <form
-    //     onSubmit={formik.handleSubmit}
-    //     >
-    //       {loginFailure && <Alert variant="danger">
-    //         Invalid login details
-    //       </Alert>}
+        {/* <button type="button" className="login-with-google-btn" disabled>
+          Sign in with Google
+        </button> */}
+        <form
+        onSubmit={formik.handleSubmit}
+        >
+          {loginFailure && <Alert variant="danger">
+            Invalid login details
+          </Alert>}
 
-    //             <h3>Sign In</h3>
+                <h3>Sign In</h3>
 
-    //             <div className="form-group">
-    //                 <label>User Name</label>
-    //                 <input type="text" className="form-control" placeholder="Enter Username"
-    //                 {...formik.getFieldProps("username")}
-    //                 />
-    //                   {formik.touched.username && formik.errors.username ? (
-    //         <div className="fv-plugins-message-container">
-    //           <div className="fv-help-block">{formik.errors.username}</div>
-    //         </div>
-    //       ) : null}
-    //             </div>
-
-    //             <div className="form-group">
-    //                 <label>Password</label>
-    //                 <input type="password" className="form-control" placeholder="Enter Password"   {...formik.getFieldProps("password")}/>
-    //                 {formik.touched.password && formik.errors.password ? (
-    //         <div className="fv-plugins-message-container">
-    //           <div className="fv-help-block">{formik.errors.password}</div>
-    //         </div>
-    //       ) : null}
-    //             </div>
-
-    //             <div className="form-group">
-    //                 <div className="custom-control custom-checkbox">
-    //                     <input type="checkbox" className="custom-control-input" id="customCheck1" />
-    //                     <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-    //                 </div>
-    //             </div>
-
-    //             <button type="submit" className="btn btn-primary btn-block">LOGIN</button>
-    //             <p className="forgot-password text-right">
-    //                 Forgot <a href="#">password?</a>
-    //             </p>
-    //         </form>
-    //         </div>
-    <div className="wrapper-login bg-image">
-    <div className="container">
-        <div className="row">
-            <div className="col-xs-1 col-xs-offset-11 volume">
-                <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked/>
-                    <label className="form-check-label" htmlFor="check1"></label>
-                  </div>
+                <div className="form-group">
+                    <label>User Name</label>
+                    <input type="text" className="form-control" placeholder="Enter Username"
+                    {...formik.getFieldProps("username")}
+                    />
+                      {formik.touched.username && formik.errors.username ? (
+            <div className="fv-plugins-message-container">
+              <div className="fv-help-block">{formik.errors.username}</div>
             </div>
-        </div>
-        <div className="row">
-            <div className="col-xs-6 col-xs-offset-3 mt">
-                <form>
-                    <div className="form-check row">
-                        <div className="col-xs-12 col-md-6">
-                            <input type="radio" className="form-check-input" id="radio1" name="optradio" value="option1" checked/>
-                            <label className="radio-inline employee-active" htmlFor="radio1">EMPLOYEE</label>
-                        </div>
-                        <div className="col-xs-12 col-md-6">
-                            <input type="radio" className="form-check-input" id="radio2" name="optradio" value="option2"/>
-                            <label className="radio-inline" htmlFor="radio2">ADMIN</label>
-                        </div>
-                      </div>
-              <div className="form-outline">
-                <div>
-                  <button type="button" onClick={googleSignin} className="google-sigin">
-                    <div className="signin-sub">
-                      <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg"><g fill="#000" fill-rule="evenodd">
-                        <path d="M9 3.48c1.69 0 2.83.73 3.48 1.34l2.54-2.48C13.46.89 11.43 0 9 0 5.48 0 2.44 2.02.96 4.96l2.91 2.26C4.6 5.05 6.62 3.48 9 3.48z" fill="#EA4335"></path>
-                        <path d="M17.64 9.2c0-.74-.06-1.28-.19-1.84H9v3.34h4.96c-.1.83-.64 2.08-1.84 2.92l2.84 2.2c1.7-1.57 2.68-3.88 2.68-6.62z" fill="#4285F4"></path>
-                        <path d="M3.88 10.78A5.54 5.54 0 0 1 3.58 9c0-.62.11-1.22.29-1.78L.96 4.96A9.008 9.008 0 0 0 0 9c0 1.45.35 2.82.96 4.04l2.92-2.26z" fill="#FBBC05"></path>
-                        <path d="M9 18c2.43 0 4.47-.8 5.96-2.18l-2.84-2.2c-.76.53-1.78.9-3.12.9-2.38 0-4.4-1.57-5.12-3.74L.97 13.04C2.45 15.98 5.48 18 9 18z" fill="#34A853"></path><path fill="none" d="M0 0h18v18H0z"></path>
-                      </g></svg></div>
-                    <span className="signin-text">Log in with Google</span>
-                  </button>
+          ) : null}
                 </div>
-              </div>
-               {/* <div className="form-outline">
-               <button onClick={signInWithGoogle} type="button" className="login-with-google-btn" ></button>
 
-               </div> */}
-                    <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form2Example1">EMAIL</label>
-                        <input type="email" id="form2Example1" className="form-control" />
-                      
-                    </div>
-                  
-                    <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form2Example2">PASSWORD</label>
-                        <input type="password" id="form2Example2" className="form-control" />
-                    </div>
-             
-                    <div className="row mb-4">
-                      <div className="col passreset">
-                        <a href="#">FORGOT PASSWORD</a>
-                      </div>
-                    </div>
-                  
-                    <button type="button" className="mb-4 signin"></button>
-                  
-                  </form>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter Password"   {...formik.getFieldProps("password")}/>
+                    {formik.touched.password && formik.errors.password ? (
+            <div className="fv-plugins-message-container">
+              <div className="fv-help-block">{formik.errors.password}</div>
             </div>
-        </div>
+          ) : null}
+                </div>
 
-      </div>
-</div>
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">LOGIN</button>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
+            </div>
+//     <div className="wrapper-login bg-image">
+//     <div className="container">
+//         <div className="row">
+//             <div className="col-xs-1 col-xs-offset-11 volume">
+//                 <div className="form-check">
+//                     <input className="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked/>
+//                     <label className="form-check-label" htmlFor="check1"></label>
+//                   </div>
+//             </div>
+//         </div>
+//         <div className="row">
+//             <div className="col-xs-6 col-xs-offset-3 mt">
+//                 <form>
+//                     <div className="form-check row">
+//                         <div className="col-xs-12 col-md-6">
+//                             <input type="radio" className="form-check-input" id="radio1" name="optradio" value="option1" checked/>
+//                             <label className="radio-inline employee-active" htmlFor="radio1">EMPLOYEE</label>
+//                         </div>
+//                         <div className="col-xs-12 col-md-6">
+//                             <input type="radio" className="form-check-input" id="radio2" name="optradio" value="option2"/>
+//                             <label className="radio-inline" htmlFor="radio2">ADMIN</label>
+//                         </div>
+//                       </div>
+//               <div className="form-outline">
+//                 <div>
+//                   <button type="button" onClick={googleSignin} className="google-sigin">
+//                     <div className="signin-sub">
+//                       <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg"><g fill="#000" fill-rule="evenodd">
+//                         <path d="M9 3.48c1.69 0 2.83.73 3.48 1.34l2.54-2.48C13.46.89 11.43 0 9 0 5.48 0 2.44 2.02.96 4.96l2.91 2.26C4.6 5.05 6.62 3.48 9 3.48z" fill="#EA4335"></path>
+//                         <path d="M17.64 9.2c0-.74-.06-1.28-.19-1.84H9v3.34h4.96c-.1.83-.64 2.08-1.84 2.92l2.84 2.2c1.7-1.57 2.68-3.88 2.68-6.62z" fill="#4285F4"></path>
+//                         <path d="M3.88 10.78A5.54 5.54 0 0 1 3.58 9c0-.62.11-1.22.29-1.78L.96 4.96A9.008 9.008 0 0 0 0 9c0 1.45.35 2.82.96 4.04l2.92-2.26z" fill="#FBBC05"></path>
+//                         <path d="M9 18c2.43 0 4.47-.8 5.96-2.18l-2.84-2.2c-.76.53-1.78.9-3.12.9-2.38 0-4.4-1.57-5.12-3.74L.97 13.04C2.45 15.98 5.48 18 9 18z" fill="#34A853"></path><path fill="none" d="M0 0h18v18H0z"></path>
+//                       </g></svg></div>
+//                     <span className="signin-text">Log in with Google</span>
+//                   </button>
+//                 </div>
+//               </div>
+//                {/* <div className="form-outline">
+//                <button onClick={signInWithGoogle} type="button" className="login-with-google-btn" ></button>
+
+//                </div> */}
+//                     <div className="form-outline mb-4">
+//                         <label className="form-label" htmlFor="form2Example1">EMAIL</label>
+//                         <input type="email" id="form2Example1" className="form-control" />
+                      
+//                     </div>
+                  
+//                     <div className="form-outline mb-4">
+//                         <label className="form-label" htmlFor="form2Example2">PASSWORD</label>
+//                         <input type="password" id="form2Example2" className="form-control" />
+//                     </div>
+             
+//                     <div className="row mb-4">
+//                       <div className="col passreset">
+//                         <a href="#">FORGOT PASSWORD</a>
+//                       </div>
+//                     </div>
+                  
+//                     <button type="button" className="mb-4 signin"></button>
+                  
+//                   </form>
+//             </div>
+//         </div>
+
+//       </div>
+// </div>
 );
 }
 
