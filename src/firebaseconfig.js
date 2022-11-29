eb0995d4f7bf03f55537c41cfb5fe742b769293f
joeljoys-official <joeljoys.official@gmail.com>
2022-11-29 17:49:21 +0530

    Prompt login

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -17,9 +17,9 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 
 export const provider = new GoogleAuthProvider();
-// provider.setCustomParameters({
-//   prompt: "select_account",
-// });
+provider.setCustomParameters({
+  prompt: "select_account",
+});
 
 export const signInWithGoogle = () => {
   signInWithPopup(auth, provider)
